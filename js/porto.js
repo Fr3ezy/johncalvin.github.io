1e6630ab5f80e77cafc0e69d5046c39de6fc761a
Fr3ezy <calvinjohn810@yahoo.com>
2023-05-13 16:04:33 +0700

    preload agar tidak dapat discroll

--- js/porto.js
+++ js/porto.js
@@ -3,12 +3,16 @@ document.onreadystatechange = function() {
     if (document.readyState !== "complete") { 
         document.querySelector( 
         "body").style.visibility = "hidden"; 
+        document.querySelector(
+        "body").style.overflow = "hidden";
         document.querySelector( 
         "#pre-loader").style.visibility = "visible"; 
     } else { 
-        document.querySelector( 
-        "#pre-loader").style.display = "none"; 
+        document.querySelector(
+        "body").style.overflow = "auto";
         document.querySelector( 
         "body").style.visibility = "visible"; 
+        document.querySelector( 
+        "#pre-loader").style.display = "none"; 
     } 
 }; 
